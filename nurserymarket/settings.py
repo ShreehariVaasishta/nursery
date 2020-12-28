7daf994e6455c8070ff4f08370a3cdd28ab9985c
ShreehariVaasishta <shreehari9481@gmail.com>
2020-12-28 21:14:55 +0530

    Make changes for deploying on heroku fix heroku procfile name

--- nurserymarket/settings.py
+++ nurserymarket/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -90,11 +90,11 @@ WSGI_APPLICATION = "nurserymarket.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": config("db_name"),
-        "USER": config("db_user"),
-        "PASSWORD": config("db_user_password"),
-        "HOST": config("host"),
-        "PORT": config("port"),
+        "NAME": os.environ.get("db_name"),
+        "USER": os.environ.get("db_user"),
+        "PASSWORD": os.environ.get("db_user_password"),
+        "HOST": os.environ.get("host"),
+        "PORT": os.environ.get("port"),
     }
 }
 
