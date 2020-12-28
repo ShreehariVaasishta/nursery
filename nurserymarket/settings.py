0d16a06f1d2a722f58715188e357f0594a9924c3
ShreehariVaasishta <shreehari9481@gmail.com>
2020-12-28 21:33:33 +0530

    allow heroku host

--- nurserymarket/settings.py
+++ nurserymarket/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["dphi-nursery-sde.herokuapp.com"]
 
 
 # Application definition
@@ -151,6 +151,7 @@ JWT_AUTH = {
 # Django Debug Toolbar
 INTERNAL_IPS = [
     "127.0.0.1",
+    "dphi-nursery-sde.herokuapp.com",
 ]
 
 # Rest Framework
